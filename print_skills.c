5e3cdfa76ff973de6118576c58ab08f84c49300c
nmke-de <github@nmke.de>
2022-06-29 23:45:28 +0200

    Added print_skills.c

---
+++ print_skills.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_skills(char **list, int len_i, int selection, int offset){
+	int row = 0, col = 0;
+	bsize(&row, &col);
+	--row;
+	int start = max(0, selection-row+1+offset);
+	int end = min(len_i, row+start);
+	for(int i = start; i < end; i++){
+		if (selection == i) bputc('>');
+		else bputc(' ');
+		bputs(list[i], 10);
+		if (selection == i) bputc('<');
+		else bputc(' ');
+		bputc('\n');
+	}
+}
